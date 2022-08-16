03e04764432a7341d17c8870c5c79fb098dbc0b5
JerDamb <gaufre.mr@gmail.com>
2022-08-16 17:18:22 +0200

    importation de l'API pokemon, mise en place des premiers éléments de la carte

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Cards from "./components/Cards";
+import axios from "axios";
 
 const App = () => {
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=20"
+  );
+
+  useEffect(() => {
+    axios.get(loadMore).then((res) => setAllPokemons(res.data.results));
+  }, []);
+
+  // const getAllPokemons = () => {
+  //   axios.get(loadMore).then((res) => setAllPokemons(res.data.results));
+  //   const data = axios.get(loadMore).then((res) => setAllPokemons(res));
+
+  //   setLoadMore(data.next);
+
+  //   const createPokemonObject = (result) => {};
+  //   createPokemonObject(data.result);
+  // };
+
   return (
     <div className="home-container">
       <h1>Pokedex</h1>
       <Cards />
+      <button className="load-more">Load more</button>
     </div>
   );
 };
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,7 @@ const Card = () => {
   return (
     <div className="card">
       <img src="" alt="" />
+      <div className="number">#937</div>
       <div className="name">Carapuce</div>
       <div className="type">Eau</div>
     </div>
